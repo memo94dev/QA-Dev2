c275677f9d8cff2db2ba99481983c26e7ae43cf9
Guillermo Home <guille.work.94@gmail.com>
2025-08-25 15:00:19 -0300

    Se agrega funcion para guardar nuevo usuario

--- utils/functions.js
+++ utils/functions.js
@@ -109,6 +109,9 @@ const codigoInput = document.getElementById('cliente-codigo');
 const idInput = document.getElementById('cliente-id');
 const nombreInput = document.getElementById('cliente-nombre');
 const emailInput = document.getElementById('cliente-email');
+const celularInput = document.getElementById('cliente-celular');
+const fechaNacInput = document.getElementById('cliente-fechaNac');
+const generoInput = document.getElementById('cliente-genero');
 
 // Cargar lista de clientes
 fetch('https://desa-backend-usuario-api.onrender.com/api/Usuarios',
@@ -125,7 +128,7 @@ fetch('https://desa-backend-usuario-api.onrender.com/api/Usuarios',
         clientes.forEach(cliente => {
             const item = document.createElement('li');
             item.className = 'list-group-item list-group-item-action';
-            item.textContent = `${cliente.nombre} (${cliente.email})`;
+            item.textContent = `${cliente.idUsuario} - ${cliente.nombre} (${cliente.email})`;
             item.onclick = () => cargarCliente(cliente.idUsuario);
             lista.appendChild(item);
         });
@@ -144,20 +147,27 @@ function cargarCliente(id) {
         .then(cliente => {
             console.log(cliente.idUsuario);
             idInput.value = cliente.idUsuario;
-            codigoInput.value = cliente.idUsuario;
             nombreInput.value = cliente.nombre;
             emailInput.value = cliente.email;
+            celularInput.value = cliente.celular;
+            fechaNacInput.value = cliente.fechaNacimiento;
+            generoInput.value = cliente.genero;
             detalle.classList.remove('d-none');
+            console.log('Cliente fecha nacimiento:', cliente.fechaNacimiento);
         });
 }
 
-// Guardar cambios
+// Actualizar Usuarios
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     const id = idInput.value;
     const datos = {
         nombre: nombreInput.value,
-        email: emailInput.value
+        email: emailInput.value,
+        email: emailInput.value,
+        celular: celularInput.value,
+        fechaNacimiento: fechaNacInput.value,
+        genero: generoInput.value
     };
 
     fetch(`https://desa-backend-usuario-api.onrender.com/api/Usuarios/${id}`, {
@@ -168,41 +178,96 @@ form.addEventListener('submit', function (e) {
         },
         body: JSON.stringify(datos)
     })
-        .then(res => res.json())
-        .then(actualizado => {
-            alert('Cliente actualizado correctamente');
-            location.reload(); // Recarga para ver cambios
+        .then(res => {
+            if (res.ok) {
+                Swal.fire({
+                    icon: "success",
+                    title: "Cliente actualizado",
+                    text: `El cliente con ID ${id} ha sido actualizado correctamente.`,
+                    footer: '<p>Recarga la página para ver los cambios</p>',
+                    timer: 1500,
+                    showConfirmButton: false
+                }).then(() => {
+                    location.reload(); // 🔁 Recarga la página después del alert
+                });
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Error al actualizar",
+                    text: `No se pudo actualizar el cliente. Código de error: ${res.status}`,
+                    footer: '<p>Inténtalo más tarde!</p>'
+                });
+            }
         });
 });
 
-// Verificar estado de la API
+    // Verificar estado de la API
 
-document.getElementById("verifyApi").addEventListener("click", function (e) {
-    e.preventDefault();
-fetch("https://desa-backend-usuario-api.onrender.com/")
-        .then(res => {
-            console.log("Codigo de estado: " + res.status);
-            const codigoRespuesta = res.status;
-            if (res.status === 200) {
-                return res.json();
-            } else {
-                throw new Error("Error en la respuesta de la API");
-            }
-        })
-        .then(data => {
-            console.log("Estado de la API:", data);
-            Swal.fire({
-                icon: "info",
-                title: "Estado de la API: " + codigoRespuesta,
-                text: `Mensaje: ${data}`,
-                footer: '<p>Se ha verificado la API</p>'
+    document.getElementById("verifyApi").addEventListener("click", function (e) {
+        e.preventDefault();
+        fetch("https://desa-backend-usuario-api.onrender.com/")
+            .then(res => res.json())
+            .then(data => {
+                console.log("Estado de la API:", data);
+                Swal.fire({
+                    icon: "info",
+                    title: "Estado de la API: " + 200,
+                    text: `Mensaje: ${data}`,
+                    footer: '<p>Se ha verificado la API</p>'
+                });
+            })
+            .catch(err => {
+                console.error("Error atrapado en catch:", err);
+                Swal.fire({
+                    icon: "error",
+                    title: "Error de Conexión ",
+                    text: `Verificá tu conexión o intentá más tarde`,
+                    footer: '<p>Verificá tu conexión o intentá más tarde</p>' + err
+                });
             });
-        })
-        .catch(err => console.error("Error atrapado en catch:", err));
-        Swal.fire({
-            icon: "error",
-            title: "Error de Conexión " + codigoRespuesta,
-            text: `Verificá tu conexión o intentá más tarde`,
-            footer: '<p>Verificá tu conexión o intentá más tarde</p>'
+    });
+
+    // Guardar Nuevo Usuario
+
+document.getElementById('cliente-form-nuevo').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const datos = {
+        nombre: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+        celular: document.getElementById('celular').value,
+        fechaNacimiento: document.getElementById('fechaNac').value + "T00:00:00Z",
+        genero: document.querySelector('input[name="genero"]:checked').value
+    };
+    console.log("Datos del nuevo usuario:", datos);
+
+    fetch("https://desa-backend-usuario-api.onrender.com/api/Usuarios", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': 'CLAVE_API_KEY_123456'
+        },
+        body: JSON.stringify(datos)
+    })
+        .then(res => {
+            if (res.ok) {
+                Swal.fire({
+                    icon: "success",
+                    title: "Usuario creado",
+                    text: `El usuario ${datos.nombre} ha sido creado correctamente.`,
+                    footer: '<p>Recarga la página para ver los cambios</p>',
+                    timer: 1500,
+                    showConfirmButton: false
+                }).then(() => {
+                    location.reload(); // Recarga la página después del alert
+                });
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Error al crear usuario",
+                    text: `No se pudo crear el usuario. Código de error: ${res.status}`,
+                    footer: '<p>Inténtalo más tarde!</p>'
+                });
+            }
         });
 });
